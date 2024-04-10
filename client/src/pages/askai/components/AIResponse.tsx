d3a8d325b8efb2d511db33995f31ef5dcb7833cc
aschouw30 <217284183@mycput.ac.za>
2024-04-10 18:28:22 +0200

    Added changes to settings page, and changed text colour to white

--- client/src/pages/askai/components/AIResponse.tsx
+++ client/src/pages/askai/components/AIResponse.tsx
@@ -5,12 +5,12 @@ type Props = {
 };
 const AIResponse: FC<Props> = ({ message }) => {
   return (
-    <div className="flex w-full items-startborder-gray-200 rounded-2xl p-2 bg-gray-800 gap-2.5">
+    <div className="flex w-full items-startborder-gray-200 rounded-2xl p-2 bg-[#2f2f2f] gap-2.5">
       <img src="/smiley.svg" alt="smiley" className="h-8 w-8" />
       <div className="flex flex-col gap-1 w-full">
         <h1 className="font-bold mb-2 mt-2">AI</h1>
         <div className="flex flex-col w-full  leading-1.5 pb-4 rounded-e-xl rounded-es-xl ">
-          <p className=" font-normal text-gray-900 dark:text-white">
+          <p className=" font-normal text-white dark:text-white">
             {message.message}
           </p>
         </div>

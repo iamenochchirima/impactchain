7055750f2c47c8f82d39ed80ef460cebdf479eb6
aschouw30 <217284183@mycput.ac.za>
2024-04-16 11:37:05 +0200

    Added all new pages and new buttons on settings page

--- client/src/pages/settings/Settings.tsx
+++ client/src/pages/settings/Settings.tsx
@@ -8,58 +8,40 @@ const Settings = () => {
     <div className="w-[216px] h-[50px] text-center text-white text-[40px] font-extrabold font-NeueMachinaUltrabold">Settings</div>
     </div>
 
-    <div className="w-full h-[853px] relative ml-5 mt-3">
-    <div className="w-full h-[853px] left-0 top-0 absolute bg-neutral-800 rounded-[56px]" />
-    <div className="w-[124px] h-1 left-[126px] top-[583px] absolute text-white text-[32px] font-normal font-['PP Telegraf']">Info</div>
-    <div className="w-[50px] h-[50px] left-[50px] top-[558px] absolute justify-center items-center inline-flex">
-        <img className="w-[50px] h-[50px]" src="https://via.placeholder.com/50x50" />
+    
+    <div className= "w-full flex flex-col">
+      {/* Grey background box*/}
+        <div className=" w-11/12 h-screen ml-5 mt-5 bg-neutral-800 rounded-[56px]">
+
+          {/* Section 1*/}
+          <div>
+          {/* Heading*/}
+            <div className='flex flex-row'>
+              <img className="w-[50px] h-[50px]" src="./user_icon.svg" />
+              <div className=" absolute text-white text-[32px] font-normal font-TelegraphRegular">Account</div>
+            </div>
+
+            <div className='flex flex-col'>
+              <div className="w-[800px] h-[50px] relative bg-black rounded-[56px]" />
+              <div className="w-[152px] h-[45px]  text-white text-xl font-normal font-TelegraphRegular">Profile</div>
+
+              
+              <div className="w-[800px] h-[50px] relative bg-black rounded-[56px]" />
+              <div className="w-[152px] h-[45px]  text-white text-xl font-normal font-['PP Telegraf']">Password</div>
+              
+
+              </div>
+          </div>
+
+          {/* Section 2*/}
+          <div></div>
+
+          {/* Section 3*/}
+          <div></div>
+          
+      </div>
+
     </div>
-    <div className="w-[124px] h-1 left-[126px] top-[281px] absolute text-white text-[32px] font-normal font-['PP Telegraf']">Admin</div>
-    <div className="w-[50px] h-[50px] left-[52px] top-[251px] absolute justify-center items-center inline-flex">
-        <img className="w-[50px] h-[50px]" src="https://via.placeholder.com/50x50" />
-    </div>
-    <div className="w-[50px] h-[50px] left-[50px] top-[49px] absolute justify-center items-center inline-flex">
-        <img className="w-[50px] h-[50px]" src="https://via.placeholder.com/50x50" />
-    </div>
-    <div className="w-[800px] h-[50px] left-[50px] top-[119px] absolute">
-        <div className="w-[800px] h-[50px] left-0 top-0 absolute bg-black rounded-[56px]" />
-        <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Dark</button>
-        <div className="w-[152px] h-[45px] left-[64px] top-[3px] absolute text-white text-xl font-normal font-['PP Telegraf']">Profile</div>
-    </div>
-    <div className="w-[800px] h-[50px] left-[50px] top-[179px] absolute">
-        <div className="w-[800px] h-[50px] left-0 top-0 absolute bg-black rounded-[56px]" />
-        <div className="w-[152px] h-[45px] left-[64px] top-[3px] absolute text-white text-xl font-normal font-['PP Telegraf']">Password</div>
-    </div>
-    <div className="w-[137px] h-1 left-[126px] top-[72px] absolute text-white text-[32px] font-normal font-['PP Telegraf']">Account</div>
-    <div className="w-[800px] h-[50px] left-[50px] top-[321px] absolute">
-        <div className="w-[800px] h-[50px] left-0 top-0 absolute bg-black rounded-[56px]" />
-        <div className="w-[152px] h-[45px] left-[64px] top-[3px] absolute text-white text-xl font-normal font-['PP Telegraf']">Brand Kit</div>
-    </div>
-    <div className="w-[800px] h-[50px] left-[50px] top-[381px] absolute">
-        <div className="w-[800px] h-[50px] left-0 top-0 absolute bg-black rounded-[56px]" />
-        <div className="w-[198px] h-[45px] left-[64px] top-[3px] absolute text-white text-xl font-normal font-['PP Telegraf']">Integrations</div>
-    </div>
-    <div className="w-[800px] h-[50px] left-[50px] top-[441px] absolute">
-        <div className="w-[800px] h-[50px] left-0 top-0 absolute bg-black rounded-[56px]" />
-        <div className="w-[275px] h-[45px] left-[64px] top-[3px] absolute text-white text-xl font-normal font-['PP Telegraf']">Your ESG Goals</div>
-    </div>
-    <div className="w-[800px] h-[50px] left-[50px] top-[501px] absolute">
-        <div className="w-[800px] h-[50px] left-0 top-0 absolute bg-black rounded-[56px]" />
-        <div className="w-[275px] h-[45px] left-[64px] top-[3px] absolute text-white text-xl font-normal font-['PP Telegraf']">Payment Account</div>
-    </div>
-    <div className="w-[800px] h-[50px] left-[50px] top-[627px] absolute">
-        <div className="w-[800px] h-[50px] left-0 top-0 absolute bg-black rounded-[56px]" />
-        <div className="w-[203px] h-[45px] left-[64px] top-[3px] absolute text-white text-xl font-normal font-['PP Telegraf']">Terms of Use</div>
-    </div>
-    <div className="w-[800px] h-[50px] left-[50px] top-[687px] absolute">
-        <div className="w-[800px] h-[50px] left-0 top-0 absolute bg-black rounded-[56px]" />
-        <div className="w-[214px] h-[45px] left-[64px] top-[3px] absolute text-white text-xl font-normal font-['PP Telegraf']">Privacy Policy</div>
-    </div>
-    <div className="w-[800px] h-[50px] left-[50px] top-[747px] absolute">
-        <div className="w-[800px] h-[50px] left-0 top-0 absolute bg-black rounded-[56px]" />
-        <div className="w-[214px] h-[45px] left-[64px] top-[3px] absolute text-white text-xl font-normal font-['PP Telegraf']">Litepaper</div>
-    </div>
-</div>
 </div>
   )
 }

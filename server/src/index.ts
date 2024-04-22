36083dfbca2ed39c1f916d70c1b1e9789f56bd69
iamenochchirima <iamenochchirima@gmail.com>
2024-04-22 04:50:16 +0200

    PORT

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(errorHandler)
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 

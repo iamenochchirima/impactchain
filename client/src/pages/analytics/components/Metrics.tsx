e705af995b6573720bdfd60bd4903ca12e5c5394
aschouw30 <217284183@mycput.ac.za>
2024-05-29 11:14:47 +0200

    Added mx to metrics, moved inline with the icon

--- client/src/pages/analytics/components/Metrics.tsx
+++ client/src/pages/analytics/components/Metrics.tsx
@@ -84,7 +84,7 @@ const Metrics: FC<Props> = ({ setOpenModal, category }) => {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 font-TelegraphRegular">
         <div className="bg-custom-gray fixed rounded-xl w-3/4 px-6 py-2 min-w-min max-w-full space-y-8">
           <div className="mb-10">
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center ">
               <img src="i.c.logo2.png" alt="logo-image" className="h-20 w-20" />
               <h3 className="text-3xl font-bold text-white mt-4 text-center font-NeueMachinaRegular">
                 {category.title}s
@@ -96,7 +96,7 @@ const Metrics: FC<Props> = ({ setOpenModal, category }) => {
             <div className="w-full">
               {displayedTargets.length > 0 ? (
                 <>
-                  <div className="grid grid-cols-3 gap-4 mt-5">
+                  <div className="grid grid-cols-3 gap-4 mt-5 mx-6">
                     {displayedTargets.map((target: TargetOption) => (
                       <ALMetricCard key={target.id} {...{ target, category }} />
                     ))}

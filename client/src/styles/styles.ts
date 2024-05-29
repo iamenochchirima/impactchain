1f7648a313419b65464e21d5287700ff1d768644
aschouw30 <217284183@mycput.ac.za>
2024-05-29 13:49:26 +0200

    Added style changes

--- client/src/styles/styles.ts
+++ client/src/styles/styles.ts
@@ -8,24 +8,24 @@ export const styles = {
   addProgramBDiv: "flex justify-end mt-4 mb-10",
   // Manual Data Form
   goalDiv: "flex justify-center items-center py-4",
-  goalTitle: "text-white text-2xl font-bold",
+  goalTitle: "text-white text-2xl font-TelegraphRegular font-bold ",
   goalInputDiv: "mb-4 flex flex-col gap-2",
   goalInput:
-    "shadow min-h-[150px] placeholder-white min-w-[150px] placeholder:text-sm placeholder:text-gray-900 rounded-3xl appearance-none border-2 border-green-500 w-full py-2 px-3 bg-gray-400 text-black leading-tight focus:outline-none focus:shadow-outline",
+    "shadow min-h-[150px] placeholder-white min-w-[150px] placeholder:text-sm placeholder:font-TelegraphUltraLight placeholder:text-gray-900 rounded-xl appearance-none border-2 border-green-500 w-full py-2 px-3 bg-gray-400 text-black leading-tight focus:outline-none focus:shadow-outline",
   munualDataForm:
-    "bg-black shadow-md font-PoppinsRegular rounded px-8 pt-6 pb-8 mb-4 mt-10 text-white",
+    "bg-black shadow-md font-TelegraphRegular rounded px-8 pt-6 pb-8 mb-4 mt-10 text-white",
   formHeader: "text-2xl text-white font-bold text-center py-3",
-  formTitle: "text-white",
-  inputDiv: "mb-4 flex flex-col gap-3",
-  textAreaInput: "shadow min-h-[100px] placeholder-white min-w-[150px] placeholder:text-sm placeholder:text-gray-900 rounded-3xl appearance-none border-2 border-green-500 w-full py-2 px-3 bg-gray-400 text-black leading-tight focus:outline-none focus:shadow-outline",
+  formTitle: "text-white font-TelegraphRegular",
+  inputDiv: "mb-4 flex flex-col gap-2",
+  textAreaInput: "shadow min-h-[100px] placeholder-white min-w-[150px] placeholder:text-sm placeholder:font-TelegraphUltraLight placeholder:text-gray-900 rounded-xl appearance-none border-2 border-green-500 w-full py-2 px-3 bg-gray-400 text-black leading-tight focus:outline-none focus:shadow-outline",
   inputLabel: "text-white",
   formInput:
-    "shadow placeholder-white min-w-[150px]  placeholder:text-sm placeholder:text-gray-900   rounded-3xl appearance-none border-2 border-green-500 w-full py-2 px-3 bg-gray-400 text-black leading-tight focus:outline-none focus:shadow-outline",
+    "shadow placeholder-white min-w-[150px]  placeholder:text-sm placeholder:text-gray-900 placeholder:font-TelegraphUltraLight  rounded-xl appearance-none border-2 border-green-500 w-full py-2 px-3 bg-gray-400 text-black leading-tight focus:outline-none focus:shadow-outline",
   formSelectInput:
     "shadow placeholder-white min-w-[150px]  placeholder:text-sm placeholder:text-gray-900   rounded-3xl  border-2 border-green-500 w-full py-2 px-3 bg-gray-400 text-black leading-tight focus:outline-none focus:shadow-outline",
   errorP: "text-red-500",
   roundedButton:
-    "bg-custom-green px-10 py-1.5  rounded-full text-black font-bold",
+    "bg-custom-green px-10 py-1.5  rounded-xl text-black font-bold",
 
   programsDiv:
     "border-x border-y border-custom-green flex flex-col gap-2 rounded p-3",
@@ -38,7 +38,7 @@ export const styles = {
   testmoCardDiv: "mb-3 flex flex-col gap-2",
   testimonialTitle: "text-white text-2xl font-bold text-center py-3",
   testimonialLabel: "text-white text-center",
-  testmoButtonDiv: "flex gap-4 justify-center items-center",
+  testmoButtonDiv: "flex gap-4 justify-center items-center ",
   testimoUploadDiv: "text-custom-green px-10 py-2 mb-4 rounded-full cursor-pointer bg-gray-700",
   testimoFileName : "text-white text-center",
   testimoCancelBtn: "",
